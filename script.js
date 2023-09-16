74095c62927e98a0a14a128d7565c2f1dee7d97a
JaleRehimli0909 <rehimlijale0909@gmail.com>
2023-09-16 17:42:14 +0400

    Making Navigation Bar Responsive

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
+const navbarMenu = document.querySelector(".navbar .links");
+const menuBtn = document.querySelector('.menu-btn');
+const hideMenuBtn = navbarMenu.querySelector('.close-btn');
 const showPopupBtn = document.querySelector(".login-btn");
 const hidePopupBtn = document.querySelector(".form-popup .close-btn");
 const formPopup = document.querySelector(".form-popup");
 const loginSignupLink = document.querySelectorAll(".form-box .button-link a");
 
+
+
+// Show mabile menu
+menuBtn.addEventListener('click', ()=>{
+     navbarMenu.classList.toggle("show-menu")
+});
+
+// Hide mobile menu
+hideMenuBtn.addEventListener('click', ()=>menuBtn.click());
+
+// Show form popup
 showPopupBtn.addEventListener('click', ()=>{
     document.body.classList.toggle("show-popup")
 });
 
+// Hide form popup
 hidePopupBtn.addEventListener('click', ()=>showPopupBtn.click());
 
 loginSignupLink.forEach(link=>{
